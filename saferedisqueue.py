94ef33c77d926de8ed6bf027c40e394cf88cad01
Fabian Neumann <fn@sturzbach.de>
2012-02-11 00:12:06 +0100

    First attempt to _autoclean method.

--- saferedisqueue.py
+++ saferedisqueue.py
@@ -7,10 +7,8 @@ A small wrapper around Redis that provides access to a FIFO queue and allows
 upstream code to mark pop'ed items as processed successfully (ACK'ing) or
 unsucessfully (FAIL'ing). Failed items are automatically requeued. Addionally
 a backup is kept for items that were neither ack'ed nor fail'ed, i.e. in case
-the consumer crashes.
-
-Note: automatic requeueing of items from the backup queue is currently not
-implemented, but a development branch already exists.
+the consumer crashes. The backup items will be requeued as soon as one of the
+consumer(s) comes up again.
 
 All actions are atomic and it is safe to have multiple producers and consumers
 accessing the same queue concurrently.
@@ -29,15 +27,51 @@ import redis
 
 class SafeRedisQueue(object):
 
-    QUEUE_KEY = 'fb_queue'
-    ACKBUF_KEY = 'fb_ackbuf'
-    ITEM_KEY_PREFIX = 'fb_item'
+    QUEUE_KEY = 'srq_queue'
+    ACKBUF_KEY = 'srq_ackbuf'
+    BACKUP = 'srq_backup'
+    BACKUP_LOCK = 'srq:lock:backup'
+    ITEM_KEY_PREFIX = 'srq_item'
+    AUTOCLEAN_TIMEOUT = 60
 
     def __init__(self, *args, **kwargs):
         self._redis = redis.StrictRedis(*args, **kwargs)
 
     def _item_key(self, uid):
         return '%s:%s' % (self.ITEM_KEY_PREFIX, uid)
+
+    def _autoclean(self):
+        if self._redis.exists(self.BACKUP_LOCK):
+            return
+        if self._redis.exists(self.BACKUP):
+            # Get the lock
+            if self._redis.setnx(self.BACKUP_LOCK, 1):
+                # There's a potential deadlock here:
+                # If we crash during the while loop, the BACKUP_LOCK will never
+                # be set to expire, blocking the creation of a new backup queue.
+                # Could be solved by setting the expire immediately, although
+                # then the guard could expire before we are done with the
+                # requeuing. Although that is very unlikely.
+                while self._redis.rpoplpush(self.BACKUP, self.QUEUE_KEY):
+                    pass
+                self._redis.pipeline()\
+                    .multi()\
+                    .renamenx(self.ACKBUF_KEY, self.BACKUP)\
+                    .expire(self.BACKUP_LOCK, self.AUTOCLEAN_TIMEOUT)\
+                    .execute()
+        else:
+            with self._redis.pipeline() as pipe:
+                try:
+                    pipe.watch(self.BACKUP_LOCK)
+                    if not pipe.exists(self.BACKUP_LOCK):
+                        pipe.multi()\
+                            .renamenx(self.ACKBUF_KEY, self.BACKUP)\
+                            .setex(self.BACKUP_LOCK, self.AUTOCLEAN_TIMEOUT, 1)\
+                            .execute()
+                    else:
+                        pipe.unwatch()
+                except redis.WatchError:
+                    pass
 
     def push_item(self, item):
         """Adds an item to the queue.
@@ -63,6 +97,7 @@ class SafeRedisQueue(object):
         Pops uid from queue and writes it to ackbuffer.
         Returns the corresponding item.
         """
+        self._autoclean()
         uid = self._redis.brpoplpush(self.QUEUE_KEY, self.ACKBUF_KEY, timeout)
         item = self.get_item(uid)
         return uid, item
@@ -74,6 +109,7 @@ class SafeRedisQueue(object):
         """
         self._redis.pipeline()\
                    .lrem(self.ACKBUF_KEY, uid)\
+                   .lrem(self.BACKUP, uid)\
                    .delete(self._item_key(uid))\
                    .execute()
 
@@ -84,6 +120,7 @@ class SafeRedisQueue(object):
         """
         self._redis.pipeline()\
                    .lrem(self.ACKBUF_KEY, uid)\
+                   .lrem(self.BACKUP, uid)\
                    .lpush(self.QUEUE_KEY, uid)\
                    .execute()
 
